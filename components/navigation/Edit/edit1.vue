4f01fec1dc9204b0ae0621dc7b542c4be151db6e
Hans Jørgen Torp <>
2019-06-17 00:19:46 +0200

    make pickimage to async

--- components/navigation/Edit/edit1.vue
+++ components/navigation/Edit/edit1.vue
@@ -1,15 +1,28 @@
 <template>
   <view class="editPhoto">
-    <button title="Pick an image from camera roll" onPress="{this._pickImage}"/>
-    <image :style="{width: 500, height: 500}" :source="{image}">
+    <button title="Pick an image from camera roll" onPress="pickImage"/>
+    <image :style="{width: 500, height: 500}" :source="{uri: 'file:///Users/hansjorgentorp/Downloads/alps-background-calm-1227520%20(1).jpg'}" >
   </view>
 </template>
 
 
 <script>
+// TODO : make this async??
 export default {
+    methods: {
+        pickImage: function () {
+            const permission = await Permissions.getAsync(Permissions.CAMERA_ROLL);
+            if (permission.status !== 'granted') {
 
-    
+                const newPermission = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+                if (newPermission.status === 'granted') {
+                 //its granted.
+                    }
+            }
+        }
+    }
+
+
   
 };
 </script>
