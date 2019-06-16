515a659952489b070fc7512429401e61cda7607b
Christer <christer.w.strom@gmail.com>
2019-06-17 00:39:07 +0200

    ExploreProfile added text and image

--- components/navigation/Shop/shop2.vue
+++ components/navigation/Shop/shop2.vue
@@ -2,11 +2,25 @@
   <HeaderBackCart :navigation = "navigation"
                     :headerTitle = "title">
     <view class="container">
+      
 
       <view class="card" :style="{ shadowColor: '#000', shadowOffset: { width: 0, height: 2 }, shadowOpacity: 0.8, shadowRadius: 2, elevation: 1, }"> 
             <view class="topText">
                 <text :style="{ fontSize: 25 }" >{{name}}</text>
             </view>
+            <view class="ProfileImagePos">
+            <!-- <view class="ProfileImage">  -->
+              <image
+          :style="{width: 100, height: 100}"
+          :source="require('../../../assets/profile.png')"
+        />
+            <!-- </view> -->
+            </view>
+
+            <view class="textBox">
+              <text class="textStyle" :style="{ fontSize: 18 }" > Swedish photographer edit alot of landscape photos and uses feelgod colors. </text>
+            </view>
+
             <view class="item1">
                 <view class="item2">
                 </view>
@@ -20,7 +34,10 @@
                 </view>
             </view>
         </view>
-
+    
+    <touchable-opacity :on-press="SeeFilter" class="btn" :style="{ shadowColor: '#000', shadowOffset: { width: 0, height: 2 }, shadowOpacity: 0.8, shadowRadius: 2, elevation: 1, }" >
+        <text :style="{ color: '#AE8D92', fontSize: '20', textAlign: 'center' }">See all filters</text>
+    </touchable-opacity>
 
     </view>
   </HeaderBackCart>
@@ -33,12 +50,18 @@ export default {
           type: Object
       }
   },
+  methods: {
+      SeeFilter: function() {
+        alert('Clicked Image')
+      }
+  },
   data() {
     return {
       title: "Profile",
       name: "Jonas Johansen",
     };
-  }
+  },
+  
 };
 </script>
 
@@ -51,7 +74,6 @@ export default {
     background-color: #fff;
     padding: 20;
     margin: 30;
-    /* padding-bottom: 100%; */
 }
 .topText {
     align-items: center;
@@ -59,10 +81,8 @@ export default {
 .item1 {
     flex-direction: row;
     justify-content: center;
-    padding: 20;
-    padding-top: 300;
-    /* padding: 100%; */
-    
+    /* padding: 20; */
+    padding-top: 20;
 }
 .item2 {
     height: 90;
@@ -70,5 +90,29 @@ export default {
     background-color: gray;
     margin-right: 5;
     margin-left: 5;
+}
+.ProfileImagePos {
+  flex-direction: row;
+    justify-content: center;
+    padding-top: 10;
+}
+.ProfileImage {
+  background-color: black;
+  height: 100;
+  width: 100;
+  border-radius: 100;
+  border-width: 1;
+  border-color: gray;
+}
+.textBox {
+  padding-top: 20;
+}
+.textStyle {
+  text-align: center;
+}
+.btn {
+  background-color: #E3D8D4;
+  padding: 18;
+  margin: 30;
 }
 </style>
