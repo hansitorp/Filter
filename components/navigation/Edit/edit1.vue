ff9b6ace2ebe2bfa830541ef36054e1ac658dd38
Hans Jørgen Torp <>
2019-06-17 00:28:44 +0200

    Fixed async

--- components/navigation/Edit/edit1.vue
+++ components/navigation/Edit/edit1.vue
@@ -1,22 +1,25 @@
 <template>
   <view class="editPhoto">
-    <button title="Pick an image from camera roll" onPress="pickImage"/>
+    <button 
+        :on-press="pickImage"
+        title="pickImage"
+    />
     <image :style="{width: 500, height: 500}" :source="{uri: 'file:///Users/hansjorgentorp/Downloads/alps-background-calm-1227520%20(1).jpg'}" >
   </view>
 </template>
 
 
 <script>
-// TODO : make this async??
+// TODO :
 export default {
     methods: {
-        pickImage: function () {
+        pickImage: async function () {
             const permission = await Permissions.getAsync(Permissions.CAMERA_ROLL);
             if (permission.status !== 'granted') {
 
                 const newPermission = await Permissions.askAsync(Permissions.CAMERA_ROLL);
                 if (newPermission.status === 'granted') {
-                 //its granted.
+                 
                     }
             }
         }
