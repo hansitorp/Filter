986b80954f4ed52e21798d748f52b9537edfd2fd
Hans Jørgen Torp <>
2019-06-17 00:47:06 +0200

    implemented image picker

--- components/navigation/Edit/edit1.vue
+++ components/navigation/Edit/edit1.vue
@@ -1,32 +1,37 @@
 <template>
   <view class="editPhoto">
-    <button 
-        :on-press="pickImage"
-        title="pickImage"
-    />
-    <image :style="{width: 500, height: 500}" :source="{uri: 'file:///Users/hansjorgentorp/Downloads/alps-background-calm-1227520%20(1).jpg'}" >
+    <button :on-press="pickImage" title="pickImage"/>
+    <image
+      :style="{width: 500, height: 500}"
+      :source="{uri: 'file:///Users/hansjorgentorp/Downloads/alps-background-calm-1227520%20(1).jpg'}"
+    >
   </view>
 </template>
 
 
 <script>
+import React from "react";
+import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
 // TODO :
 export default {
-    methods: {
-        pickImage: async function () {
-            const permission = await Permissions.getAsync(Permissions.CAMERA_ROLL);
-            if (permission.status !== 'granted') {
-
-                const newPermission = await Permissions.askAsync(Permissions.CAMERA_ROLL);
-                if (newPermission.status === 'granted') {
-                 
-                    }
-            }
+  methods: {
+    pickImage: async function() {
+      const permission = await Permissions.getAsync(Permissions.CAMERA_ROLL);
+      if (permission.status !== "granted") {
+        const newPermission = await Permissions.askAsync(
+          Permissions.CAMERA_ROLL
+        );
+        if (newPermission.status === "granted") {
         }
+      }
+
+      let result = await ImagePicker.launchImageLibraryAsync({
+        allowsEditing: false,
+        aspect: [4, 3]
+      });
     }
-
-
-  
+  }
 };
 </script>
 
